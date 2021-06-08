d290faba4dc7d825a0979ebf805234669f55512f
Anton Drozdov <iacade.drozdov@gmail.com>
2021-06-08 07:26:05 +0300

    Refactor auth service

--- pages/api/login.js
+++ pages/api/login.js
@@ -1,31 +1,62 @@
-import fetchJson from "../../lib/fetchJson";
+import bcrypt from "bcrypt";
 import withSession from "../../lib/session";
-import {client} from "./sqlHelper/client"
-const { Pool, Client } = require('pg')
-
+import { client } from "./sqlHelper/client"
 
 export default withSession(async (req, res) => {
-  const { username , password } = await req.body;
-  const char = "'";
-  let query = 'SELECT username, email from accounts where password like ' + char +  password + char +' and username  like ' + char + username+ char ;
-  try {
-    await client.connect();
-    const result = await client.query({
-      rowMode: 'array',
-      text: query,
-    });
-    client.end();
-    if(result.rowCount!==1){
-      throw {response:{status:418},data:{message:"Invalid password or login!"}};
+    const {
+        username,
+        password: purePassword
+    } = await req.body;
+    const query = `
+        SELECT
+            username,
+            email,
+            password
+        FROM
+            accounts
+        WHERE
+            username LIKE '${ username }'
+    `;
+
+    try {
+        await client.connect();
+        
+        const result = await client.query({
+            rowMode: 'array',
+            text: query,
+        });
+
+        client.end();
+
+        if (result.rowCount !== 1) {
+            throw {
+                response: { status: 418 },
+                data: { message: "Invalid password or login!" }
+            };
+        }
+
+        const [ username, email, password ] = result.rows[0];
+        const isMatched = await bcrypt.compare(purePassword, password);
+
+        if (!isMatched) {
+            throw {
+                response: { status: 418 },
+                data: { message: "Invalid password or login!" }
+            };
+        }
+
+        const user = {
+            isLoggedIn: true,
+            login: username,
+            email: email
+        };
+
+        req.session.set("user", user);
+        await req.session.save();
+
+        res.json(user);
+    } catch (error) {
+        const { response: fetchResponse } = error;
+        res.status(fetchResponse?.status || 500).json(error.data);
     }
-    let email = result.rows[0][1];
-    let login = result.rows[0][0];
-    let user = { isLoggedIn: true, login, email };
-    req.session.set("user", user);
-    await req.session.save();
-    res.json(user);
-  } catch (error) {
-    const { response: fetchResponse } = error;
-    res.status(fetchResponse?.status || 500).json(error.data);
-  }
 });
--- pages/api/register.js
+++ pages/api/register.js
@@ -1,37 +1,71 @@
-import fetchJson from "../../lib/fetchJson";
+import bcrypt from "bcrypt";
 import withSession from "../../lib/session";
-import {client} from "./sqlHelper/client"
-const { Pool, Client } = require('pg')
+import { client } from "./sqlHelper/client"
 
+const HASH_SALT_ROUNDS = 7;
 
 export default withSession(async (req, res) => {
-  const { username , email ,password } = await req.body;
-  const char = "'";
-  let queryInsert = 'INSERT INTO accounts(username,email,password,created_on,last_login) VALUES( '+char+username+char+' , ' + char +email+ char+' , ' + char+password+char+' , (SELECT NOW()), (SELECT NOW()))';
-  let querySelect = 'SELECT username, email from accounts where email like ' + char +  email + char +' or username  like ' + char + username+ char ;
-  try {
-    await client.connect();
-    const result = await client.query({
-      rowMode: 'array',
-      text: querySelect,
-    });
-    if(result.rowCount!==0){
-      throw {response:{status:418},data:{message:"Invalid username or email"}};
+    const {
+        username,
+        email,
+        password
+    } = await req.body;
+    const hashedPassword = await bcrypt.hash(password, HASH_SALT_ROUNDS);
+    const queryInsert = `
+        INSERT INTO accounts (username, email, password, created_on, last_login)
+        VALUES ('${ username }', '${ email }', ${ hashedPassword }, NOW(), NOW())
+    `;
+    const querySelect = `
+        SELECT
+            username,
+            email
+        FROM
+            accounts
+        WHERE
+            email LIKE '${ email }'
+        OR
+            username LIKE '${ username }'
+    `;
+
+    try {
+        await client.connect();
+
+        const result = await client.query({
+            rowMode: 'array',
+            text: querySelect,
+        });
+
+        if (result.rowCount !== 0) {
+            throw {
+                response: { status: 418 },
+                data: { message: "Invalid username or email" }
+            };
+        }
+
+        await client.query({
+            rowMode: 'array',
+            text: queryInsert,
+        });
+
+        client.end();
+
+        const [ login, email ] = result.rows[0];
+        const user = {
+            isLoggedIn: true,
+            login: login,
+            email: email
+        };
+        
+        req.session.set("user", user);
+        await req.session.save();
+
+        res.json(user);
+    } catch (error) {
+        console.log(error);
+
+        const { response: fetchResponse } = error;
+
+        res.status(fetchResponse?.status || 500)
+            .json(error.data);
     }
-    await client.query({
-      rowMode: 'array',
-      text: queryInsert,
-    });
-    client.end();
-    let email = result.rows[0][1];
-    let login = result.rows[0][0];
-    let user = { isLoggedIn: true, login, email };
-    req.session.set("user", user);
-    await req.session.save();
-    res.json(user);
-  } catch (error) {
-    console.log(error);
-    const { response: fetchResponse } = error;
-    res.status(fetchResponse?.status || 500).json(error.data);
-  }
 });
